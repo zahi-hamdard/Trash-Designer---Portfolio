47179ffc82e0934366493d519c67ea4b43774b03
Zahid Hamdard <zahi.hamdard@gmail.com>
2025-03-09 23:52:10 +0430

    Add files via upload
    **"Trash Designer Repos"**  
    Bold, creative projects turning chaos into treasure. Explore and collaborate!

---
+++ public/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.burger_menu').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
